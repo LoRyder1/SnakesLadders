c53b4f315d09d41679944bb13b05b752967685a7
drmyersii <davidrmyersii@gmail.com>
2016-04-02 15:49:19 -0400

    Adding constructor for Space class. Adding type and logic to determine the amount of spaces a Space object can move a Player.

--- lib/space.rb
+++ lib/space.rb
@@ -1,2 +1,19 @@
 class Space
+    attr_reader :type, :spaces_to_move
+
+    def initialize type
+        # make sure passed type is in allowed types list
+        case type
+        when 'snake', 'ladder', 'normal'
+            @type = type
+        else
+            @type = 'normal'
+        end
+
+        if @type == 'snake' || @type == 'ladder'
+            @spaces_to_move = rand(1..10)
+        else
+            @spaces_to_move = 0
+        end
+    end
 end
