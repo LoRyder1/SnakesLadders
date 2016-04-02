252f35d590abd11412ab6bcaead8cb2ca1626a15
drmyersii <davidrmyersii@gmail.com>
2016-04-02 17:04:26 -0400

    Fixing instance variable references.

--- lib/user.rb
+++ lib/user.rb
@@ -1,21 +1,21 @@
 class User
-  attr_reader :current_space, :name
+  attr_reader :position, :name
   def initialize name
-    @current_space = 0
+    @position = 0
     @name = name
   end
 
   def move num_of_spaces
-    if @current_space + num_of_spaces <= 100
-      @current_space += num_of_spaces
+    if @position + num_of_spaces <= 100
+      @position += num_of_spaces
     end
   end
 
   def move_back num_of_spaces
-    if current_space - num_of_spaces < 0
-      current_space = 0
+    if @position - num_of_spaces < 0
+      @position = 0
     else
-      current_space -= num_of_spaces
+      @position -= num_of_spaces
     end
   end
 
